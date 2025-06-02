ea6e2476f8663749617c55f97934aa38fc545c70
Pedro-Jordao <phjordao2522@gmail.com>
2025-06-02 13:29:15 -0300

    Correcao Aura e Outro

--- validador.py
+++ validador.py
@@ -22,8 +22,8 @@ def detect_flag(card_number):
         (r'^4[0-9]{12}(?:[0-9]{3})?(?:[0-9]{3})?$', 'Visa', [13, 16, 19]),
         # American Express
         (r'^3[47][0-9]{13}$', 'American Express', [15]),
-        # Diners Club
-        (r'^(3(0[0-5]|[689])[0-9]{11})$', 'Diners Club', [14]),
+        # Diners Club (corrigido)
+        (r'^(3(0[0-5]|6|8|9|880)[0-9]{11})$', 'Diners Club', [14]),
         # Discover
         (r'^(6011[0-9]{12}|65[0-9]{14}|64[4-9][0-9]{13}|622(12[6-9]|1[3-9][0-9]|[2-8][0-9]{2}|9[01][0-9]|92[0-5])[0-9]{10})$', 'Discover', [16, 19]),
         # EnRoute
@@ -34,8 +34,8 @@ def detect_flag(card_number):
         (r'^8699[0-9]{11}$', 'Voyager', [15]),
         # Hipercard
         (r'^606282[0-9]{10}$', 'Hipercard', [16]),
-        # Aura
-        (r'^50787796[0-9]{8,11}$', 'Aura', [16, 17, 18, 19]),
+        # Aura (corrigido)
+        (r'^(5018|5020|5031|5041|5054|5066|5067|5093|5019)[0-9]{12,15}$', 'Aura', [16, 17, 18, 19]),
     ]
 
     for pattern, flag, lengths in rules:
@@ -58,10 +58,10 @@ if __name__ == "__main__":
         "3530111333300000",  # JCB
         "869900000000000",   # Voyager
         "6062825624254001",  # Hipercard
-        "5078779600000000",  # Aura
+        "5093986995399521",  # Aura (exemplo que você trouxe)
+        "30140431002441",    # Diners Club (exemplo que você trouxe)
         "1234567890123456",  # Inválido
     ]
     # for card in test_cards:
     #     print(f"{card}: {detect_flag(card)}")
-
-    print (detect_flag("5093 9869 9539 9521"))  # MasterCard válido
+print(detect_flag("5040 8305 7439 0739"))  # Teste específico para o exemplo que você trouxe
